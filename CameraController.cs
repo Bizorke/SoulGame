4455c1a701e0b95ebfa966722f35f728a83f34c4
liv1985 <olivia.i.gajadhar@gmail.com>
2019-03-24 23:33:46 -0400

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Portal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate((Portal.transform.position - transform.position) * 0.1f);
+	}
+}
---
+++ GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	
+	public GameObject Soul;
+	public GameObject BadSoul;
+
+	public GameObject PlayButton;
+
+	public GameObject ScoreDisplay;
+
+	public GameObject Portal;
+
+	public GameObject ProgressBar;
+
+	public GameObject NotificationPopupController;
+
+	public GameObject MusicLevelOne;
+
+	private float score = 0;
+
+	private float maxScore = 20;
+
+	private bool gameActive = false;
+
+	private float arenaWidth = 25;
+
+	private LevelController LevelController;
+
+	public void GivePoint()
+	{
+		score = score + 1;
+		ProgressBar.GetComponent<Slider>().value = score / maxScore;
+		ScoreDisplay.GetComponent<Text>().text = "Score: " + score;
+
+		LevelController.GivePoint();
+	}
+
+	// Use this for initialization
+	void Start () {
+		LevelController = new LevelController(this, Soul, BadSoul);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameActive){
+			LevelController.Update();
+		}
+		
+	}
+
+	public void PlayButtonClick()
+	{
+		gameActive = true;
+		PlayButton.SetActive(false);
+		Portal.SetActive(true);
+		ProgressBar.SetActive(true);
+		ScoreDisplay.SetActive(true);
+		ScoreDisplay.GetComponent<Text>().text = "Score: 0";
+		MusicLevelOne.GetComponent<AudioSource>().Play();
+		Notify("Level 1");
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	public void Notify(string text)
+	{
+		NotificationPopupController.GetComponent<NotificationPopupController>().DisplayMessage(text);
+	}
+}
